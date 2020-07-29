c74c5d77c4f8251cc826d30e45c0f1dc80b7100d
jameskim <kcy0142@naver.com>
2020-07-29 16:16:46 +0900

    Update home_screen.dart
    상세페이지 네비게이션 추가

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:open_dart/models/corporation_response.dart';
 import 'package:open_dart/services/dart_open_api_service.dart';
 import 'package:open_dart/widgets/corporation_card.dart';
+import 'corp_detail_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key key}) : super(key: key);
@@ -29,15 +30,25 @@ class _HomeScreenState extends State<HomeScreen> {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             CorporationResponse res = snapshot.data;
-            return ListView.separated(
+             return ListView.separated(
               physics: AlwaysScrollableScrollPhysics(),
               padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
               itemBuilder: (context, index) {
-                return CorporationCard(
-                  corporation: res.list[index],
+                return InkWell(
+                  child: CorporationCard(
+                    corporation: res.list[index],
+                  ),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CorpDetailScreen(res.list[index].corpCode),
+                      ),
+                    );
+                  },
                 );
               },
-              itemCount: 10,
+              itemCount: res.list.length,
               separatorBuilder: (context, index) {
                 return SizedBox(height: 16);
               },
